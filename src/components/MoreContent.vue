81380f4b1d65b893b9f0538330ed1330a54968f2
ArisaTaki <570490640@qq.com>
2025-02-26 15:51:43 +0800

    更新个人信息

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="more-content">
-    难过的话，就吃东西，烦恼的话，就看电视，无聊的话，就睡觉。
+    你好你好，我是八六，一个前端工程师。
     <br />
-    喔，你用电脑打开了这个页面，那就看看这个吧
+    下面是我的详细信息👇🏻
     <br />
 
     本人供职于一家日本企业，期间为久保田，野村证券，国有铁道，XFLAG等团队提出过技术方案和代码贡献，爱好是前沿技术。
@@ -11,13 +11,13 @@
     技术栈有：
     <br />
 
-    前端：精通HTML5，tailwindcss，react，typescript，JavaScript，vue3，Nodejs，webpack，Nextjs，vite，WebGL
+    前端：精通HTML5，tailwindcss，react，typescript，JavaScript，vue3，Nodejs，webpack，Nextjs，vite，WebGL，LangchainJS
     <br />
 
-    后端：精通Go，php，MySQL，SqlServer，Redis，MongoDB 运维：精通linux系统，docker
+    后端也略懂一二：Go，php，MySQL，SqlServer，Redis，MongoDB 运维：精通linux系统，docker
     <br />
 
-    工具：精通git，vscode，idea，PS，AE，Blender 精通git，正在学习LangChain和Shader着色器编辑。
+    工具：精通git，vscode，idea，Cursor，figma，Notion，slack
   </div>
 </template>
 
